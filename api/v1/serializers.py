7cc3b8c09e5b87f9afc0dbbd0dc048277ce604b4
Hoang Nguyen Le Thai <hoangnlt2@fpt.com>
2025-10-20 23:21:39 +0700

    feat(api): allow creating and updating categories

--- api/v1/serializers.py
+++ api/v1/serializers.py
@@ -1,5 +1,14 @@
 from rest_framework import serializers
+from api.models import Category
 
 class CategorySerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
+
+    def create(self, validated_data):
+        return Category.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        return instance
--- api/v1/views.py
+++ api/v1/views.py
@@ -1,18 +1,41 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-
 from api.models import Category
 from api.v1.serializers import CategorySerializer
 
-@api_view()
+@api_view(['GET', 'POST'])
 def category_list(request):
-    categories = Category.objects.all()
-    serializer = CategorySerializer(categories, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.error)
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
 def category_detail(request, pk):
-    category = Category.objects.get(pk=pk)
-    serializer = CategorySerializer(category)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        category = Category.objects.get(pk=pk)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        category = Category.objects.get(pk=pk)
+        serializer = CategorySerializer(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+    if request.method == 'DELETE':
+        category = Category.objects.get(pk=pk)
+        category.delete()
+        return Response()
+
+                      
