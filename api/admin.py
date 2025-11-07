843893553d1b797aa3ea47165c32ecbdb6ec0cc9
branlabs <tinhoangadl@gmail.com>
2025-11-07 23:56:00 +0700

    Add user CRUD API

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from api.models import Category
+from api.models import Category, Transaction, User
 
 # Register your models here
 admin.site.register(Category)
+admin.site.register(Transaction)
+admin.site.register(User)
